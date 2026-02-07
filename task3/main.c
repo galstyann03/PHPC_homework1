8fd37f5a8d94930250988d4363ba0200dadcb87f
Narek Galstyan <narekgalstyanh@gmail.com>
2026-02-07 16:21:37 +0400

    task3

--- task3/main.c
+++ task3/main.c
@@ -1,3 +1,19 @@
-//
-// Created by Narek on 2/7/2026.
-//
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 10, b = 20;
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
